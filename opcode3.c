d3dc71275e348fc33ff1a1678073cfff0a85515f
hajer <hajeer_00@hotmail.com>
2023-12-13 15:09:28 -0800

    free

---
+++ opcode3.c
@@ -0,0 +1,18 @@
+#include "monty.h"
+
+/**
+ * free_stack - function that free stack
+ * @stack: the head of the list
+ */
+
+
+void free_stack(stack_t *stack)
+{
+	while (stack != NULL)
+	{
+		stack_t *temp = stack;
+
+		stack = stack->next;
+		free(temp);
+	}
+}
